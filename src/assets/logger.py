74d395f72b696f78a69e4d633fa024202d134f4c
Michael Dennis <mdennis281@gmail.com>
2019-09-26 06:09:17 -0500

    Comments & Logging Improvements

--- src/assets/logger.py
+++ src/assets/logger.py
@@ -1,9 +1,14 @@
+# Author: Michael Dennis (https://github.com/mdennis281)
+# Created: 09-26-2019
+# Project Repository: https://github.com/mdennis281/Python-Cloudflare-DNS-External-IP-Synchronizer
+# License: MIT (https://en.wikipedia.org/wiki/MIT_License)
+
 from assets.config import load as config
 import time as t
 
 def log(message):
     if config()['general']['loggingEnabled']:
-        time = t.strftime('%Y-%m-%d %H:%M:%S',t.localtime())
+        time = t.strftime('%Y-%m-%d %H:%M:%S',t.time())
         f = open(config()['general']['logPath'],'a')
         f.write(time+'\t'+message+'\n')
         f.close()
