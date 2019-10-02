cb970740b5a309e82434b08ae368d88c57c02834
Michael Dennis <mdennis281@gmail.com>
2019-10-01 19:45:36 -0500

    Logger outputs in local timezone

--- src/assets/logger.py
+++ src/assets/logger.py
@@ -8,7 +8,7 @@ import time as t
 
 def log(message):
     if config()['general']['loggingEnabled']:
-        time = t.strftime('%Y-%m-%d %H:%M:%S',t.time())
+        time = t.strftime('%Y-%m-%d %H:%M:%S',t.localtime())
         f = open(config()['general']['logPath'],'a')
         f.write(time+'\t'+message+'\n')
         f.close()
