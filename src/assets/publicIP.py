a661ae50d892b2c918bd977a45dbd29616437c39
Michael Dennis <mdennis281@gmail.com>
2019-10-01 19:46:23 -0500

    Improvements to getting IP address

--- src/assets/publicIP.py
+++ src/assets/publicIP.py
@@ -2,16 +2,38 @@
 # Created: 09-26-2019
 # Project Repository: https://github.com/mdennis281/Python-Cloudflare-DNS-External-IP-Synchronizer
 # License: MIT (https://en.wikipedia.org/wiki/MIT_License)
-
-import requests, re
+from assets.logger import log
+import requests, re, random
 
 #Uses regex to parse webpage for the external IP address
+#Has multiple hosts to check ip against, if all hosts fail to return an IP
+#it will then raise an exception.
 def get():
-    try:
-        page = requests.get('http://checkip.dyndns.com')
-        regex = '\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b'
-        ip = re.search(regex,page.text).group(0)
-        return ip
-    except Exception as err:
-        log(repr(err))
-        raise err
+    def makeCall(source):
+        r = requests.get(source)
+        r.raise_for_status()
+        return r.text
+
+    sources = [
+        'http://checkip.dyndns.com',
+        'https://api.ipify.org',
+        'https://ip.seeip.org/'
+    ]
+
+    ip = ''
+
+    # While IP address has not been found and there
+    # are still sources that have not been used.
+    while (not ip) and sources:
+        url = sources.pop(random.randint(0,len(sources)-1))
+        try:
+            page = makeCall(url)
+            regex = '\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b' #regex matching IP address
+            ip = re.search(regex,page).group(0)
+            return ip
+        except Exception as err:
+            log('Unable to parse IP from: ' + url +' | '+repr(err))
+
+    e = 'NETWORK ERROR: Could not determine public IP.'
+    log(e)
+    raise Exception(e)
